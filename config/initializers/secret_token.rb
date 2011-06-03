516abbd810342984ba27a287b3c4b8bbe9110df8
Mauricio M. de Almeida <mauriciomdea@gmail.com>
2011-06-02 23:06:34 -0300

    First commit!

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Brandnewblog::Application.config.secret_token = 'ff168a11e7a62bd191d7219bc92153292d537436bb605f2a8796a22cce91b010f13bdc6a786ccf60a42bf24821f2550360b12f305e6ad17a28adf9a2508a7200'
