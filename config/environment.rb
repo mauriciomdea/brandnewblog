1488b57c1d610b0f31300466fdc25179d9bd3c00
Mauricio M. de Almeida <mauriciomdea@gmail.com>
2011-06-09 17:59:42 -0300

    Changed dateformat display.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,6 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Brandnewblog::Application.initialize!
+
+Time::DATE_FORMATS[:date_en] = "%m/%d/%Y - %I:%M%p"
+Time::DATE_FORMATS[:date_pt_br] = "%d/%m/%Y - %H:%M"
